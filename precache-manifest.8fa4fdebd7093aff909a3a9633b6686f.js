0cd0e95e440a6a4a57764054bee11c93b14cc9ac
Alexey Kabanov <al.k96@mail.ru>
2020-02-08 18:21:35 +0300

    Updates

---
+++ precache-manifest.8fa4fdebd7093aff909a3a9633b6686f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fdf9d0420b6af09e318f10bb635278e",
+    "url": "/rick_morty/index.html"
+  },
+  {
+    "revision": "ac3a23760161fca20dc3",
+    "url": "/rick_morty/static/css/main.c3cbdaae.chunk.css"
+  },
+  {
+    "revision": "6265d224c8be3491dadb",
+    "url": "/rick_morty/static/js/2.e2d381d7.chunk.js"
+  },
+  {
+    "revision": "c6967699ed8f6cf33744a1f8a8d37ba5",
+    "url": "/rick_morty/static/js/2.e2d381d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac3a23760161fca20dc3",
+    "url": "/rick_morty/static/js/main.de48bf37.chunk.js"
+  },
+  {
+    "revision": "e57efc82f327db810f61",
+    "url": "/rick_morty/static/js/runtime-main.c1b30ce9.js"
+  }
+]);
